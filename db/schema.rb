8d2fa72b4fe398751b10246a3e31e9e585a3c391
Emival Luiz da Silva Junior <emival@gmail.com>
2016-06-08 17:34:56 -0300

    Implementation of Professional inclusion

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522024801) do
-
+ActiveRecord::Schema.define(version: 20160526003344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
