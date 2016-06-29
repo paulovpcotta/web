ffc54eaf7afc7cbde6561b0c80278cdd48e638af
vlbgomes <vlbgomes@gmail.com>
2016-06-28 21:53:53 -0300

    adicionando coluna description em professional_profession

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622010753) do
+ActiveRecord::Schema.define(version: 20160629005117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -140,6 +140,7 @@ ActiveRecord::Schema.define(version: 20160622010753) do
     t.integer  "profession_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "description"
   end
 
   create_table "professional_services", force: :cascade do |t|
